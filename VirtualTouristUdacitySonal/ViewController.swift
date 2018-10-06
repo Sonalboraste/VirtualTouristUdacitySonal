a7e01dd4cec8560220675e65eb3b5f5ebec640e5
Sonalboraste <sonalboraste@gmail.com>
2018-10-06 14:23:05 -0500

    Initial Commit

---
+++ VirtualTouristUdacitySonal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTouristUdacitySonal
+//
+//  Created by Sonal on 10/6/18.
+//  Copyright © 2018 Sonal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
